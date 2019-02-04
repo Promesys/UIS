2f0cbb70f6a69235e96a0ea7a2a4a0cc7746b7c0
Chris <chris@promesys.nl>
2019-02-04 21:22:54 +0100

    .

--- includes/timezone.sh
+++ includes/timezone.sh
@@ -1,8 +1,10 @@
-		# Set timezone
-		echo "Setting timezone"
-		echo ''
-		sudo dpkg-reconfigure tzdata
-		echo "Installing NTP time servers"
-		echo ''
-		sudo apt-get install ntp -y  &>/dev/null
-        exit 0
+#!/bin/bash		
+        
+# Set timezone
+echo "Setting timezone..."
+echo ''
+sudo dpkg-reconfigure tzdata
+echo "Installing NTP time servers..."
+echo ''
+sudo apt-get install ntp -y  &>/dev/null
+exit 0
--- includes/update.sh
+++ includes/update.sh
@@ -1,12 +1,14 @@
-		# Run an update and upgrade for packages
-		echo "Checking for available software updates"
-		echo ''
-		sudo apt-get update -y &>/dev/null
-		echo "Applying critical updates"
-		echo ''
-		sudo apt-get upgrade -y  &>/dev/null
-        # Install essential dependencies
-		echo "Installing essential dependencies"
-		echo ''
-		sudo apt-get install -y build-essential  &>/dev/null
-        exit 0
+#!/bin/bash
+
+# Run an update and upgrade for packages
+echo "Checking for available software updates..."
+echo ''
+sudo apt-get update -y &>/dev/null
+echo "Applying critical updates..."
+echo ''
+sudo apt-get upgrade -y  &>/dev/null
+# Install essential dependencies
+echo "Installing essential dependencies..."
+echo ''
+sudo apt-get install -y build-essential  &>/dev/null
+exit 0
