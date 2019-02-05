44c6e41ab812d788027f43f9776a1500278f4281
Chris <chris@promesys.nl>
2019-02-05 11:14:58 +0100

    update

--- uid-update.sh
+++ uid-update.sh
@@ -1,6 +1,7 @@
 git fetch origin
 git reset --hard origin/master
 chmod +x ./uid-start.sh
+chmod +x ./uid-update.sh
 chmod +x ./includes/autoupdate.sh
 chmod +x ./includes/nginx.sh
 chmod +x ./includes/timezone.sh
