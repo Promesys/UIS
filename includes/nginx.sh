b365ef3189a876a4a691aafde1c0fa83e0a0472e
Chris <chris@promesys.nl>
2019-02-05 10:55:52 +0100

    php

--- includes/nginx.sh
+++ includes/nginx.sh
@@ -3,5 +3,20 @@
 # Install NGINX
 echo "Installing NGINX..."
 echo ''
-sudo apt update
-sudo apt install nginx -y
+# Install software-properties-common package to give us add-apt-repository package
+sudo apt-get install -y software-properties-common
+# Install latest nginx version from community maintained ppa
+sudo add-apt-repository ppa:nginx/stable
+# Update packages after adding ppa
+sudo apt-get update
+# Install nginx
+sudo apt-get install -y nginx
+# Tweak Nginx settings
+sed -i "s/worker_processes.*/worker_processes auto;/" /etc/nginx/nginx.conf
+sed -i "s/# multi_accept.*/multi_accept on;/" /etc/nginx/nginx.conf
+sed -i "s/# server_names_hash_bucket_size.*/server_names_hash_bucket_size 128;/" /etc/nginx/nginx.conf
+sed -i "s/# server_tokens off/server_tokens off/" /etc/nginx/nginx.conf
+# Check status
+sudo service nginx
+# Start nginx if it is not already running
+sudo service nginx start
--- includes/php.sh
+++ includes/php.sh
@@ -3,9 +3,40 @@
 # Install PHP
 echo "Installing PHP..."
 echo ''
-apt install software-properties-common python-software-properties
-LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
-apt update
-apt install php7.3 php7.3-cli php7.3-common
+# Install latest nginx version from community maintained ppa
+sudo apt-get install python-software-properties -y
+sudo add-apt-repository ppa:ondrej/nginx -y
+sudo apt-get update
+# Install PHP 7.3
+apt install php7.3 php7.3-cli php7.3-common -y
 apt install php-pear php7.3-curl php7.3-dev php7.3-gd php7.3-mbstring php7.3-zip php7.3-mysql php7.3-xml php7.3-fpm libapache2-mod-php7.3 php7.3-imagick php7.3-recode php7.3-tidy php7.3-xmlrpc php7.3-intl
+apt install php7.3-fpm -y
+update-alternatives --set php /usr/bin/php7.3
+# Update PHP CLI configuration
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.3/cli/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.3/cli/php.ini
+sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.3/cli/php.ini
+sed -i "s/;date.timezone.*/date.timezone = Europe/Amsterdam/" /etc/php/7.3/cli/php.ini
+# Tweak PHP-FPM settings
+sed -i "s/error_reporting = .*/error_reporting = E_ALL \& ~E_NOTICE \& ~E_STRICT \& ~E_DEPRECATED/" /etc/php/7.2/fpm/php.ini
+sed -i "s/display_errors = .*/display_errors = Off/" /etc/php/7.3/fpm/php.ini
+sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.3/fpm/php.ini
+sed -i "s/upload_max_filesize = .*/upload_max_filesize = 256M/" /etc/php/7.3/fpm/php.ini
+sed -i "s/post_max_size = .*/post_max_size = 256M/" /etc/php/7.3/fpm/php.ini
+sed -i "s/;date.timezone.*/date.timezone = Europe/Amsterdam/" /etc/php/7.3/fpm/php.ini
+# Tune PHP-FPM pool settings
+sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/;request_terminate_timeout.*/request_terminate_timeout = 60/" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/pm\.max_children.*/pm.max_children = 70/" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/pm\.start_servers.*/pm.start_servers = 20/" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/pm\.min_spare_servers.*/pm.min_spare_servers = 20/" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/pm\.max_spare_servers.*/pm.max_spare_servers = 35/" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/;pm\.max_requests.*/pm.max_requests = 500/" /etc/php/7.3/fpm/pool.d/www.conf
+
+sudo systemctl restart nginx.service
+sudo systemctl restart php7.3-fpm.service
+
+#Remove PHP7.2 Packages
+sudo apt-get purge `dpkg -l | grep php7.2| awk '{print $2}' |tr "\n" " "` -y
+
 php -v
